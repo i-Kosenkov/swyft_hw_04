0984b071ed4dd9d539b47418d95ba304d406422e
Kosenkov <9588434@gmail.com>
2023-06-13 12:14:25 +0300

    Task 2 done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,7 +8,22 @@ enum Pizza: String{
 }
 
 // 2. Создать структуру пиццы, она должна содержать стоимость, вид пиццы, толстое или тонкое тесто и добавки (например, дополнительно добавить пепперони, помидоры, сыр). Вид пиццы должен быть вложенным типом для структуры пиццы. Подсказка: добавки лучше также сделать перечислением.
-
+struct PizzaStruct{
+    enum PizzaType: String{
+        case pizza1 = "Margherita"
+        case pizza2 = "Carbonara"
+        case pizza3 = "Napoletana"
+        case pizza4 = "Romana"
+        case pizza5 = "Prosciutto"
+    }
+    var cost: Double
+    var thinDough: Bool
+    enum PizzaSuplements: String{
+        case supl1 = "Peperroni"
+        case supl2 = "Tomato"
+        case supl3 = "Cheese"
+    }
+}
 
 // 3. Создать класс пиццерии, добавить массив с возможными видами пиццы. Переменная с массивом должна быть приватной. Массив задаётся в инициализаторе.
 
