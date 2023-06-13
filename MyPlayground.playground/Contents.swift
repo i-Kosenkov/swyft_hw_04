1e99adf56f677a8b3cac57d3d11713ac102c2b70
Kosenkov <9588434@gmail.com>
2023-06-13 13:44:45 +0300

    Task 3,4,5 done Не смог понять как создать функцию для получения всех доступных пицц И как при наличии добавок к пицце в виде перечисления сделать возможность добавления нескольких добавок к элементу класса?

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -16,20 +16,41 @@ struct PizzaStruct{
         case pizza4 = "Romana"
         case pizza5 = "Prosciutto"
     }
-    var cost: Double
-    var thinDough: Bool
     enum PizzaSuplements: String{
         case supl1 = "Peperroni"
         case supl2 = "Tomato"
         case supl3 = "Cheese"
+        case none
     }
+    var name: PizzaType
+    var cost: Double
+    var thinDough: Bool
+    var suplements: PizzaSuplements
 }
 
 // 3. Создать класс пиццерии, добавить массив с возможными видами пиццы. Переменная с массивом должна быть приватной. Массив задаётся в инициализаторе.
-
-
 // 4. Написать в классе пиццерии функции для добавления нового вида пиццы и для получения всех доступных пицц.
+class Cafe{
+    private var pizza: [PizzaStruct] = []
+    init (pizza: [PizzaStruct]){
+        self.pizza = pizza
+    }
+    func addPizza(pizza: PizzaStruct){
+        self.pizza.append(pizza)
+    }
+//    func viewAllPizza(){
+//        print(self)
+//    }
+}
 
 
 // 5. Создать экземпляр класса пиццерии и добавить в него несколько видов пицц.
+let margherita = PizzaStruct(name: PizzaStruct.PizzaType.pizza1, cost: 100, thinDough: true, suplements: .none)
+let romana = PizzaStruct(name: .pizza4, cost: 150, thinDough: false, suplements: .supl3)
+let napoletana = PizzaStruct(name: .pizza3, cost: 200, thinDough: true, suplements: .none)
+var cafe = Cafe(pizza: [margherita, romana, napoletana])
 
+let carbonara = PizzaStruct(name: .pizza2, cost: 300, thinDough: true, suplements: .supl2)
+cafe.addPizza(pizza: carbonara)
+
+//cafe.viewAllPizza()
